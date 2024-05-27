4d81671e53948549775b6a3ba1cbdd7ba8e3422d
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-05-27 17:55:11 +0800

    [6] Add app bar
    Adding app bar to sign up page for back button to go back to login page

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -12,6 +12,9 @@ class AppTheme {
 
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: AppPallete.backgroundColor,
+    appBarTheme: const AppBarTheme(
+      backgroundColor: AppPallete.backgroundColor,
+    ),
     inputDecorationTheme: InputDecorationTheme(
       contentPadding: const EdgeInsets.all(30),
       enabledBorder: _border(),
