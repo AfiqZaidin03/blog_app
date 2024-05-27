39538e74b97c669327d98663c337f02adf3c34e9
AfiqZaidin <76638759+AfiqZaidin@users.noreply.github.com>
2024-05-27 17:30:13 +0800

    [3] Add text controller to authfield and obscureText to password
    Adding text controller to authfield for name, email and password and obscureText to password

--- lib/features/presentation/pages/signup_page.dart
+++ lib/features/presentation/pages/signup_page.dart
@@ -1,3 +1,4 @@
+import 'package:blog_app/core/theme/app_pallete.dart';
 import 'package:blog_app/features/presentation/widgets/aut_gradient_button.dart';
 import 'package:blog_app/features/presentation/widgets/auth_field.dart';
 import 'package:flutter/material.dart';
@@ -10,30 +11,73 @@ class SignUpPage extends StatefulWidget {
 }
 
 class _SignUpPageState extends State<SignUpPage> {
+  final nameController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    nameController.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(15.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Sign Up.',
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                'Sign Up.',
+                style: TextStyle(
+                  fontSize: 50,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 30),
-            AuthField(hintText: 'Name'),
-            SizedBox(height: 15),
-            AuthField(hintText: 'Email'),
-            SizedBox(height: 15),
-            AuthField(hintText: 'Password'),
-            SizedBox(height: 20),
-            AutGradientButton(),
-          ],
+              const SizedBox(height: 30),
+              AuthField(
+                hintText: 'Name',
+                controller: nameController,
+              ),
+              const SizedBox(height: 15),
+              AuthField(
+                hintText: 'Email',
+                controller: emailController,
+              ),
+              const SizedBox(height: 15),
+              AuthField(
+                hintText: 'Password',
+                controller: passwordController,
+                isObscureText: true,
+              ),
+              const SizedBox(height: 20),
+              const AutGradientButton(),
+              const SizedBox(height: 20),
+              RichText(
+                text: TextSpan(
+                  text: 'Don\'t have an account? ',
+                  style: Theme.of(context).textTheme.titleMedium,
+                  children: [
+                    TextSpan(
+                      text: 'Sign In',
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                            color: AppPallete.gradient2,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
