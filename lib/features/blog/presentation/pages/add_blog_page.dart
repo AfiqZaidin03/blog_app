e0f78f6425e4f9c1c3864c834ebc30f2a6c28495
AfiqZaidin <muhd.afiq.zaidin03@gmail.com>
2024-06-14 17:27:06 +0800

    [47] Pick image logic and style for change image and cancel image

--- lib/features/blog/presentation/pages/add_blog_page.dart
+++ lib/features/blog/presentation/pages/add_blog_page.dart
@@ -51,38 +51,53 @@ class _AddNewBlogPageState extends State<AddNewBlogPage> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              GestureDetector(
-                onTap: () {
-                  selectImage();
-                },
-                child: DottedBorder(
-                  color: AppPallete.borderColor,
-                  dashPattern: const [10, 4],
-                  radius: const Radius.circular(10),
-                  borderType: BorderType.RRect,
-                  strokeCap: StrokeCap.round,
-                  child: Container(
-                    width: double.infinity,
-                    height: 150,
-                    child: const Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(
-                          Icons.folder_open,
-                          size: 40,
-                        ),
-                        SizedBox(height: 15),
-                        Text(
-                          'Select your image',
-                          style: TextStyle(
-                            fontSize: 15,
+              image != null
+                  ? GestureDetector(
+                      onTap: selectImage,
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 150,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.file(
+                            image!,
+                            fit: BoxFit.cover,
                           ),
-                        )
-                      ],
+                        ),
+                      ),
+                    )
+                  : GestureDetector(
+                      onTap: () {
+                        selectImage();
+                      },
+                      child: DottedBorder(
+                        color: AppPallete.borderColor,
+                        dashPattern: const [10, 4],
+                        radius: const Radius.circular(10),
+                        borderType: BorderType.RRect,
+                        strokeCap: StrokeCap.round,
+                        child: Container(
+                          width: double.infinity,
+                          height: 150,
+                          child: const Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Icons.folder_open,
+                                size: 40,
+                              ),
+                              SizedBox(height: 15),
+                              Text(
+                                'Select your image',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-                ),
-              ),
               const SizedBox(height: 20),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
